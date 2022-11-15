2e88efb087d35e05f39359fe4ce8eda53a3f0d1d
Renef13 <renefricardo30@gmail.com>
2022-11-15 00:45:34 -0300

    peguei dois codigos de exmplo

---
+++ Atividades/ponteirosatv.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void troca_valores(int *ptrx, int *ptry)
+
+{
+    int auxiliar;
+
+    // auxiliar recebe o conteúdo apontado por ptrx
+    auxiliar = *ptrx;
+
+    // coloca o valor que está no local apontado por ptry em ptrx
+    *ptrx = *ptry;
+
+    // finalmente, ptry recebe o valor armazenado em auxiliar
+    *ptry = auxiliar;
+
+    return;
+}
+
+int main()
+{
+    int a, b;
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo valor: ");
+    scanf("%d", &b);
+
+    printf("Voce digitou os valores na seguinte ordem: %d e %d\n", a, b);
+    troca_valores(&a, &b);
+
+    // Exibindo os valores já trocados após a chamada da função
+    printf("Os valores trocados sao: %d e %d\n", a, b);
+    
+    char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
+    char *elemento;
+    elemento = &alfabeto[0];
+
+    for (int i = 0; i < strlen(alfabeto); i++)
+    {
+        printf("%c \n", *elemento);
+        elemento++;
+    }
+}
